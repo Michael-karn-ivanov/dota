d0b3fd5706ed102e471532e032db108165c70fb6
Maskus <Maskus>
2015-12-20 18:51:19 +0300

    a

--- R-BradleyT/Winner_loser_mix.R
+++ R-BradleyT/Winner_loser_mix.R
@@ -1,4 +1,5 @@
 library(readr)
+library(BradleyTerry2)
 
 matches <- read.csv("matches.csv", stringsAsFactors=FALSE)
 
@@ -8,3 +9,9 @@ matches[matches$winner == "radiant", "winner_id"] <- matches[matches$winner == "
 matches$loser_id <- NA
 matches[matches$winner == "dire", "loser_id"] <- matches[matches$winner == "dire", "radiant_id"]
 matches[matches$winner == "radiant", "loser_id"] <- matches[matches$winner == "radiant", "dire_id"]
+
+levels(matches[,"winner_id"]) <- unique(c(matches[,"winner_id"], matches[,"loser_id"]))
+levels(matches[,"loser_id"]) <- unique(c(matches[,"winner_id"], matches[,"loser_id"]))
+
+summary(dotamatch.model <- BTm(player1 = winner_id, player2 = loser_id, data = matches))
+
