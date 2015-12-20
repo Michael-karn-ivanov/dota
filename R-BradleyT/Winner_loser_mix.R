ebf1df9b7c7e9b07cc277918b8f5d4770306bc17
Mikhail Ivanov <mivanov@microsoft.com>
2015-12-20 21:33:05 +0200

    Added prob extraction to Winner_loser_mix

--- R-BradleyT/Winner_loser_mix.R
+++ R-BradleyT/Winner_loser_mix.R
@@ -1,5 +1,14 @@
 library(readr)
 library(BradleyTerry2)
+library(reshape)
+
+inv_logit <- function(p) {
+  exp(p) / (1 + exp(p))
+}
+
+prob_BT <- function(ability_1, ability_2) {
+  inv_logit(ability_1 - ability_2)
+}
 
 matches <- read.csv("matches.csv", stringsAsFactors=FALSE)
 
@@ -20,8 +29,7 @@ matches <- matches[!is.na (matches$loser_id),]
 
 matches <- matches[matches$loser_id %in% matches$winner_id, ]
 matches <- matches[matches$winner_id %in% matches$loser_id, ]
-length(unique(matches$winner_id))
-length(unique(matches$loser_id))
+
 
 winner.frame <- as.data.frame(cbind(matches$winner_id, matches$match_id))
 loser.frame <- as.data.frame(cbind(matches$loser_id, matches$match_id))
@@ -36,6 +44,15 @@ loser.frame$id <- as.factor(loser.frame$id)
 summary(dotamatch.model <- BTm(result, player1 = winner.frame, 
                                player2 = loser.frame, data = matches, 
                                id = "id"))
+
+dotamatch.output <- data.frame(BTabilities(dotamatch.model))
+team_names <- rownames(dotamatch.output)
+dotamatch.abilities <- dotamatch.output$ability
+names(dotamatch.abilities) <- team_names
+
+dota_probs <- outer(dotamatch.abilities, dotamatch.abilities, prob_BT)
+diag(dota_probs) <- 0
+dota_probs <- melt(dota_probs)
 
 
 
